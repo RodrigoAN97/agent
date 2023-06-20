7736affbc52c007302eb48663ad1a7918d4665db
RodrigoAN97 <rodrigo_neves97@outlook.com>
2023-06-20 18:29:24 -0300

    input to search

--- src/components/Agents/Agents.tsx
+++ src/components/Agents/Agents.tsx
@@ -5,6 +5,7 @@ import { IAgent } from "../../types/Agent";
 import axios from "axios";
 import "./Agents.css";
 import { Link } from "react-router-dom";
+import debounce from "lodash.debounce";
 
 const Agents: FC = () => {
   const [agents, setAgents] = useState<IAgent[]>([]);
@@ -17,13 +18,27 @@ const Agents: FC = () => {
     fetchInitialData();
   }, []);
 
+  const handleSearch = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log((event.target as HTMLInputElement).value);
+  };
+
+  const debounceOnChange = debounce(handleSearch, 500);
+
   return (
     <>
-      <Link to="/join">
-        <button type="button" id="join">
-          Join the team
-        </button>
-      </Link>
+      <div id="top">
+        <Link to="/join">
+          <button type="button" id="join">
+            Join the team
+          </button>
+        </Link>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search"
+          onChange={debounceOnChange}
+        />
+      </div>
       <div className="agents">
         {agents.map((agent) => (
           <Agent key={agent.id} agent={agent} />
